e98837e05c13a954e97c09c3e84e3d30099d8f2b
Stephen Marz <stephen.marz@utk.edu>
2020-07-18 17:35:27 -0400

    Pretty up the place.

--- cosine.c
+++ cosine.c
@@ -260,7 +260,7 @@ double cos_table_1_LERP(double x)
     double i = x * 1.0;
     int index = (int)i;
     double v1 = costable_1[index];
-    double v2 = costable_1[(index + 1)];
+    double v2 = costable_1[index + 1);
     double w = i - index;
     return lerp(w, v1, v2);
 }
@@ -284,7 +284,7 @@ double cos_table_0_01_LERP(double x)
     double i = x * 100.0;
     int index = (int)i;
     double v1 = costable_0_01[index];
-    double v2 = costable_0_01[(index + 1)];
+    double v2 = costable_0_01[index + 1];
     double w = i - index;
     return lerp(w, v1, v2);
 }
@@ -296,7 +296,7 @@ double cos_table_0_001_LERP(double x)
     double i = x * 1000.0;
     int index = (int)i;
     double v1 = costable_0_001[index];
-    double v2 = costable_0_001[(index + 1)];
+    double v2 = costable_0_001[index + 1];
     double w = i - index;
     return lerp(w, v1, v2);
 }
@@ -308,7 +308,7 @@ double cos_table_0_0001_LERP(double x)
     double i = x * 10000.0;
     int index = (int)i;
     double v1 = costable_0_0001[index];
-    double v2 = costable_0_0001[(index + 1)];
+    double v2 = costable_0_0001[index + 1];
     double w = i - index;
     return lerp(w, v1, v2);
 }
